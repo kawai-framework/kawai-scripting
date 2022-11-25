f5655e976ac6ead377c7391a610773fc6b2a8e18
Yuki Arimo <emojik2006@gmail.com>
2022-11-25 16:05:28 +0100

    First commit

---
+++ kwc/kawai-config.js
@@ -0,0 +1,42 @@
+export const SiteModel = [{
+    "sidebar": "true",
+    "sidebarConfig": [{
+        "logo": "Test",
+        "links": [{
+            "Test1": "#",
+            "Test2": "#"
+        }]
+    }],
+    "topbar": "true",
+    "topbarConfig": [{
+        "logo": "Test",
+        "links": [{
+            "Test1": "#",
+            "Test2": "#"
+        }]
+    }],
+    "blocks": [{
+        "Render": ["1"]
+    }]
+}]
+
+// Aviliable code: ['Block-l', 'HTML']
+export const SiteBlocks = [{
+    "1": [{
+        "blockl": [{
+            "config": ["la"],
+            "content": ["Hello world"]
+        }]
+    }],
+    "2": [{
+        "blockl": [{
+            "config": ["la"],
+            "content": ["Welcome to kawai builder"]
+        }]
+    }]
+}]
+
+/*
+    COPYRIGHT BY YUKI ARIMO
+    KAWAI FRAMEWORK
+*/
